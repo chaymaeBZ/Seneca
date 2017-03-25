4f4ec7893ff4d15f475818744ac74a1211c0a06d
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-03-25 14:00:13 +0000

    0.1.0

--- lib/seneca.rb
+++ lib/seneca.rb
@@ -3,9 +3,7 @@ require "seneca/version"
 module Seneca
   class Application
     def call(env)
-      run proc {
-        [200, {'Content-type' => 'text/html'}, ["This is working!! Seneca is awesome :) - Ruby"]]
-      }
+      [200, {'Content-type' => 'text/html'}, ["This is working!! Seneca is awesome :) - Ruby"]]
     end
   end
 end

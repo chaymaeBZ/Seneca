6a330400338a1802140c48201a48ed997c61f376
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-05-12 23:41:52 +0100

    rescue stack too deep inside const_missing

--- lib/seneca/dependencies.rb
+++ lib/seneca/dependencies.rb
@@ -1,7 +1,12 @@
 class Object
   def self.const_missing(c)
+    return nil if @calling_const_missing
     require Seneca.to_underscore(c.to_s)
-    puts "const missing called c.to_s"
-    Object.const_get(c)
+    @calling_const_missing = true
+    #puts "const missing called c.to_s"
+    klass = Object.const_get(c)
+    @calling_const_missing = false
+
+    klass
   end
 end
